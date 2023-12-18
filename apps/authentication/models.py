d456a2fba214a0d5852ac706ed0c2b7b10243bcc
abd-Kabir <kabirumarov@gmail.com>
2023-12-18 17:42:12 +0500

    user, email + isactive unique

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from config.models import BaseModel
+from config.utils.api_exceptions import APIValidation
 
 
 class VerifyCode(BaseModel):
@@ -17,9 +18,16 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
-    email = models.EmailField(_("email address"), unique=True, blank=True)
+    email = models.EmailField(_("email address"), blank=True)
     middle_name = models.CharField(_("middle name"), max_length=150, blank=True, null=True)
     phone_number = models.CharField(_("phone number"), max_length=19, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        user = User.objects.filter(email=self.email, is_active=True)
+        if user:
+            raise APIValidation("This email is already registered")
+
+        super().save(*args, **kwargs)
 
     class Meta:
         db_table = 'User'
