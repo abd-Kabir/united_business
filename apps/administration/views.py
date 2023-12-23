e38a079b906e5b74b81fd0770b9e4cc7b2eae74f
abd-Kabir <kabirumarov@gmail.com>
2023-12-23 18:04:30 +0500

    content no pagination

--- apps/administration/views.py
+++ apps/administration/views.py
@@ -3,7 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 
 from apps.administration.models import AboutUs, Direction, Partner, Service
 from apps.administration.serializer import AboutUsSerializer, DirectionSerializer, PartnerSerializer, ServiceSerializer
-from config.pagination import APIPagination
+# from config.pagination import APIPagination
 from config.utils.permissions import LandingPage
 
 
@@ -12,7 +12,7 @@ class AboutUsModelViewSet(ModelViewSet):
     serializer_class = AboutUsSerializer
     parser_classes = (MultiPartParser,)
     permission_classes = [LandingPage, ]
-    pagination_class = APIPagination
+#     pagination_class = APIPagination
 
 
 class ServiceModelViewSet(ModelViewSet):
@@ -20,7 +20,7 @@ class ServiceModelViewSet(ModelViewSet):
     serializer_class = ServiceSerializer
     parser_classes = (MultiPartParser,)
     permission_classes = [LandingPage, ]
-    pagination_class = APIPagination
+#     pagination_class = APIPagination
 
 
 class DirectionModelViewSet(ModelViewSet):
@@ -28,7 +28,7 @@ class DirectionModelViewSet(ModelViewSet):
     serializer_class = DirectionSerializer
     parser_classes = (MultiPartParser,)
     permission_classes = [LandingPage, ]
-    pagination_class = APIPagination
+#     pagination_class = APIPagination
 
 
 class PartnerModelViewSet(ModelViewSet):
@@ -36,4 +36,4 @@ class PartnerModelViewSet(ModelViewSet):
     serializer_class = PartnerSerializer
     parser_classes = (MultiPartParser,)
     permission_classes = [LandingPage, ]
-    pagination_class = APIPagination
+#     pagination_class = APIPagination
