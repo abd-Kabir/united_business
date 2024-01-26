85d9130d3d86daa4a982d252c1fb35c5116c547d
abd-Kabir <kabirumarov@gmail.com>
2024-01-26 23:24:58 +0500

    mapping

--- apps/mapping/urls.py
+++ apps/mapping/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from apps.mapping.views import CountryListAPIView, CountryRetrieveAPIView
+from apps.mapping.views import CountryListAPIView, CountryRetrieveAPIView, CountryClassifierAPIView
 
 app_name = 'mapping'
 urlpatterns = [
-    path('country/classifier/', CountryListAPIView.as_view(), name='country_list'),
+    path('country/classifier/', CountryClassifierAPIView.as_view(), name='country_classifier'),
     path('country/list/', CountryListAPIView.as_view(), name='country_list'),
     path('country/<int:pk>/', CountryRetrieveAPIView.as_view(), name='country_retrieve'),
 ]
