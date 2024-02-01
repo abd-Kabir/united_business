81a10d3c4b1aa3a6d18324b93f5cc32c0073b83d
abd-Kabir <kabirumarov@gmail.com>
2024-02-01 09:59:49 +0500

    Guide urls

--- apps/administration/urls.py
+++ apps/administration/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
 
 from apps.administration.views import AboutUsModelViewSet, DirectionModelViewSet, PartnerModelViewSet, \
-    ServiceModelViewSet, BannerModelViewSet
+    ServiceModelViewSet, BannerModelViewSet, GuideModelViewSet
 
 app_name = 'administration'
 router = DefaultRouter()
@@ -10,5 +10,6 @@ router.register(r'direction', DirectionModelViewSet, basename='direction')
 router.register(r'partner', PartnerModelViewSet, basename='partner')
 router.register(r'service', ServiceModelViewSet, basename='service')
 router.register(r'banner', BannerModelViewSet, basename='banner')
+router.register(r'guide', GuideModelViewSet, basename='guide')
 
 urlpatterns = router.urls
