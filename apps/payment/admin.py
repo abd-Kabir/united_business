f7f4d874f0e67996aa21b090a8fbca54874da4e0
abd-Kabir <kabirumarov@gmail.com>
2024-02-09 16:12:26 +0500

    admin register

--- apps/payment/admin.py
+++ apps/payment/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.payment.models import Transaction, Subscription, Access
+
+admin.site.register(Transaction)
+admin.site.register(Subscription)
+admin.site.register(Access)
